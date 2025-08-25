b7ab80be6d9fafa8132bf268802f9f739b39ee34
Batuhan Kıskaç <kiskacbatuhan@gmail.com>
2025-08-25 19:18:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/auth.ts
+++ src/auth.ts
@@ -1,23 +1,21 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { redirect } from 'next/navigation';
 
-export const {handlers, signIn, signOut, auth} = NextAuth({
-  secret: "secret",
+export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
-    strategy: "jwt",
+    strategy: 'jwt',
   },
   providers: [
     Credentials({
       name: 'Password',
       credentials: {
-        password: {label: 'Password', type: 'password'},
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        if (credentials?.password === "admin") {
-          // Any object returned will be saved in `user` property of the JWT
-          return {id: '1', name: 'Admin'};
+        if (credentials?.password === 'admin') {
+          return { id: '1', name: 'Admin' };
         }
-        // If you return null then an error will be displayed advising the user to check their details.
         return null;
       },
     }),
@@ -26,35 +24,32 @@ export const {handlers, signIn, signOut, auth} = NextAuth({
     signIn: '/login',
   },
   callbacks: {
-    authorized({auth, request}) {
-      const {pathname} = request.nextUrl;
+    authorized({ auth, request }) {
+      const { pathname } = request.nextUrl;
       if (pathname.startsWith('/admin')) {
         return !!auth;
       }
       return true;
     },
-    jwt({token, user}) {
+    jwt({ token, user }) {
       if (user) {
         token.id = user.id;
       }
       return token;
     },
-    session({session, token}) {
+    session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string;
       }
       return session;
     },
-    redirect({url, baseUrl}) {
-      // Allows relative callback URLs
+    async redirect({ url, baseUrl }) {
       if (url.startsWith('/')) {
         return `${baseUrl}${url}`;
       }
-      // Allows callback URLs on the same origin
       if (new URL(url).origin === baseUrl) {
         return url;
       }
-      
       return baseUrl + '/admin';
     },
   },
